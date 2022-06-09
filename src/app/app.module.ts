e1a713385471eb0034981d288e3ff0a4cb4f5abf
pipe <felipe.parra@blackmind.cl>
2022-06-09 17:15:23 -0400

    change fleboxfrid en archivo angular.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//add material
+
 import { MatSliderModule } from '@angular/material/slider';
+import { MatPseudoCheckboxModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { MatSliderModule } from '@angular/material/slider';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    MatPseudoCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
