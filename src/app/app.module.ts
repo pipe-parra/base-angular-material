6667a9fbf0e4e066866dc0cd5070e4c703b96122
pipe <felipe.parra@blackmind.cl>
2022-06-12 01:52:33 -0400

    instale npm i flexboxgrid, luego arregle la relacion de aspecto para que el ancho width quede al 100%

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//add material
 
+//add moduls material
 import { MatSliderModule } from '@angular/material/slider';
 import { MatPseudoCheckboxModule } from '@angular/material/core';
 
