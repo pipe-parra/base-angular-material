00ae276d09eab3d825a38ac12005a4c022c1eb65
pipe <felipe.parra@blackmind.cl>
2022-06-12 02:40:40 -0400

    agregue en core/services/auth el servicio auth y genere los 3 metodos para registrar, logear y logout en el servicio

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
+// import { User } from '../services/user';
+import * as auth from 'firebase/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/compat/firestore';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private afa: AngularFireAuth,
+  ) { }
+
+  registerUser(email:string, password:string): Promise<any>{
+    return this.afa.createUserWithEmailAndPassword(email,password);
+  }
+  loginUser(email:string, password:string): Promise<any>{
+    return this.afa.signInWithEmailAndPassword(email,password);
+  }
+  logoutUser(): Promise<void>{
+    return this.afa.signOut();
+  }
+
 }
