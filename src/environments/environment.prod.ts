33a1a8d934f18d8a1d397722b7dad5b19293f060
pipe <felipe.parra@blackmind.cl>
2022-06-12 01:58:45 -0400

    copie las variables de entorno de firebase en el prod.ts y el ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCJ3T6kJtbsH7c_xTgEViMXKkFapMkH6Uo",
+    authDomain: "landing-material-tusumas.firebaseapp.com",
+    projectId: "landing-material-tusumas",
+    storageBucket: "landing-material-tusumas.appspot.com",
+    messagingSenderId: "1008571146290",
+    appId: "1:1008571146290:web:98991dacfd671303081c2b",
+    measurementId: "G-1LJ67Y9D5L"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCJ3T6kJtbsH7c_xTgEViMXKkFapMkH6Uo",
+    authDomain: "landing-material-tusumas.firebaseapp.com",
+    projectId: "landing-material-tusumas",
+    storageBucket: "landing-material-tusumas.appspot.com",
+    messagingSenderId: "1008571146290",
+    appId: "1:1008571146290:web:98991dacfd671303081c2b",
+    measurementId: "G-1LJ67Y9D5L"
+  }
+  
+
 };
 
 /*
