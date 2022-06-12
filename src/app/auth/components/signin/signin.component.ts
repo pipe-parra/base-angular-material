c7537cef5a0664025b54cf5d996750b9e5f70461
pipe <felipe.parra@blackmind.cl>
2022-06-12 03:04:24 -0400

    arreglamos el signin.ts agregamos async y await en la funcion

--- src/app/auth/components/signin/signin.component.ts
+++ src/app/auth/components/signin/signin.component.ts
@@ -8,13 +8,30 @@ import { AuthService } from 'src/app/core/services/auth/auth.service'; //se supo
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
-  signinForm: FormGroup | undefined;
+  signinForm!: FormGroup;
   
 
   constructor(
     private formBuilder:FormBuilder,
     private authService:AuthService
   ) { }
+
+  buildForm(): void {
+    this.signinForm = this.formBuilder.group({
+      email:['',[Validators.required, Validators.email]],
+      password:['',[Validators.required, Validators.minLength(6)]],
+    })
+  }
+  async signinUser(event: Event):Promise<void>{
+    event.preventDefault();
+    const {email, password} = this.signinForm.value;
+    try {
+      const userCredencials = await this.authService.loginUser(email, password);
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
 
   ngOnInit(): void {
   }
