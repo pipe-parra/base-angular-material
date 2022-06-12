6068968283ba570a7350a475e521f4a4258ebcd5
pipe <felipe.parra@blackmind.cl>
2022-06-12 03:14:47 -0400

    lo mismo en el formulario de registro

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/services/auth/auth.service'; //se supone que con el cmabio quye hice en tsconfig.app.json al agregar los apths deberia solo poner '@core' en vez de 'src/app/core'
 
 @Component({
   selector: 'app-register',
@@ -6,8 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder:FormBuilder,
+    private authService:AuthService
+  ) { }
+
+  buildForm(): void {
+    this.registerForm = this.formBuilder.group({
+      email:['',[Validators.required, Validators.email]],
+      password:['',[Validators.required, Validators.minLength(6)]],
+      confirmpassword:['',[Validators.required, Validators.minLength(6)]]
+    })
+  }
+  async registerUser(event: Event):Promise<void>{
+    event.preventDefault();
+    const {email, password,confirmpassword } = this.registerForm.value;
+    try {
+      if(password === confirmpassword){
+      const userCredencials = await this.authService.registerUser(email, password)
+      }else{
+        alert('Las Contraseñas no son Iguales');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
 
   ngOnInit(): void {
   }
