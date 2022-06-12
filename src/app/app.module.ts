d9daacf9da158e682d54bf0f42ed0d8fee5da0ed
pipe <felipe.parra@blackmind.cl>
2022-06-12 02:09:18 -0400

    aca comienzan las dudas porque no es igual a la nueva forma variaciones como AngularFireAuth ya no lo ocupa, usa otro provideAuth y getAuth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatPseudoCheckboxModule } from '@angular/material/core';
 
+//add firebase config
+import { environment } from 'src/environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp } from 'firebase/app';
+import { Firestore } from 'firebase/firestore';
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -18,7 +26,11 @@ import { MatPseudoCheckboxModule } from '@angular/material/core';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
-    MatPseudoCheckboxModule
+    MatPseudoCheckboxModule,
+    provideFirebaseApp(()=>initializeApp(environment.firebaseConfig)),
+    provideAuth(()=>getAuth()),
+    Firestore
+
   ],
   providers: [],
   bootstrap: [AppComponent]
