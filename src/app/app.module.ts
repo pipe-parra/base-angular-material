d87c1e73418a81720f103dd34c2529a5e3913596
pipe <felipe.parra@blackmind.cl>
2022-06-12 02:16:20 -0400

    agregue los modulos Shared y el Core y agregue comentarios al app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+//add modulo core
+import {CoreModule } from './core/core.module';
+//add module shared
+import { SharedModule } from './shared/shared.module';
 //add moduls material
 import { MatSliderModule } from '@angular/material/slider';
 import { MatPseudoCheckboxModule } from '@angular/material/core';
-
 //add firebase config
 import { environment } from 'src/environments/environment';
-import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideAuth, getAuth } from '@angular/fire/auth'; //esto cambia mucho en relacion a mis tutoriales aqui tengo dudas, antes era Angular FireAuth
 import { provideFirebaseApp } from '@angular/fire/app';
 import { initializeApp } from 'firebase/app';
-import { Firestore } from 'firebase/firestore';
+import { Firestore } from 'firebase/firestore'; //este tengo dudas si sirva para algo
 
 
 @NgModule({
@@ -29,7 +31,7 @@ import { Firestore } from 'firebase/firestore';
     MatPseudoCheckboxModule,
     provideFirebaseApp(()=>initializeApp(environment.firebaseConfig)),
     provideAuth(()=>getAuth()),
-    Firestore
+    
 
   ],
   providers: [],
